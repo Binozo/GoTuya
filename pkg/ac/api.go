6deebfb9c60ee7f3d66307cdac5c8308f3f39c89
Binozo <entwickler@binozoworks.de>
2024-08-18 22:30:12 +0200

    Added fan intensity support for AC

--- pkg/ac/api.go
+++ pkg/ac/api.go
@@ -3,6 +3,7 @@ package ac
 import (
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 func (a *AC) IsOn() (bool, error) {
@@ -58,4 +59,39 @@ func (a *AC) SetTemperature(temperature int) error {
 		onDpsIndex:          true,
 		temperatureDpsIndex: temperature,
 	})
+}
+
+// SetFanIntensity sets the fan intensity on a scale between 1 and 4.
+// 1 is low,
+// 4 is high
+func (a *AC) SetFanIntensity(intensity int) error {
+	if intensity < 1 || intensity > 4 {
+		return errors.New("intensity must be between 1 and 4")
+	}
+	if !a.IsConnected() {
+		if err := a.Connect(); err != nil {
+			return err
+		}
+		defer a.Disconnect()
+	}
+	return a.Set(map[string]interface{}{
+		onDpsIndex:           true,
+		fanIntensityDpsIndex: intensity,
+	})
+}
+
+// GetFanIntensity gets the current fan intensity on a scale between 1 and 4.
+func (a *AC) GetFanIntensity() (int, error) {
+	if !a.IsConnected() {
+		if err := a.Connect(); err != nil {
+			return 0, err
+		}
+		defer a.Disconnect()
+	}
+	currentStatus := a.GetCurrentStatus()
+	if intensityValue, ok := currentStatus[fanIntensityDpsIndex]; ok {
+		return strconv.Atoi(intensityValue.(string))
+	} else {
+		return 0, errors.New(fmt.Sprintf("dps index %s not contained in: %s", onDpsIndex, currentStatus))
+	}
 }
--- pkg/ac/constants.go
+++ pkg/ac/constants.go
@@ -2,3 +2,4 @@ package ac
 
 const onDpsIndex = "1"
 const temperatureDpsIndex = "2"
+const fanIntensityDpsIndex = "5" // Ranges from 1 to 4
